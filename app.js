98ea57b55af1b111b1a84b327550850fd44c5efb
kamlesh-00 <mistrykamlesh312@gmail.com>
2020-07-06 11:18:02 +0530

    Challenge 1,2,3,4 completed

--- app.js
+++ app.js
@@ -10,10 +10,31 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+
+app.get('/',function(req,res){
+  res.render('home',{homeStartingContent: homeStartingContent});
+});
+
+app.get('/about',function(req,res){
+  res.render('about');
+});
+
+app.get('/compose',function(req,res){
+  res.render('compose');
+});
+
+app.get('/post',function(req,res){
+  res.render('post');
+});
+
+app.get('/contact',function(req,res){
+  res.render('contact');
+});
+
 
 
 
